927028f42f743e615be0f844bd7913a26c8a7675
DPirate <milindd1996@gmail.com>
2021-07-11 17:08:16 +0530

    Post CRA setup: adding fetch api wrapper function

---
+++ src/api/api.ts
@@ -0,0 +1,48 @@
+import { ParsedUrlQueryInput } from 'querystring';
+import { stringify } from 'querystring';
+import { API_URL } from './api.config';
+
+export async function api<TResult, TBody>({
+  path,
+  body,
+  query = undefined,
+  method,
+  baseUrl = API_URL,
+}: {
+  path: string;
+  body?: TBody;
+  query?: ParsedUrlQueryInput;
+  method: 'get' | 'post' | 'put' | 'patch' | 'delete';
+  baseUrl?: string;
+}): Promise<TResult> {
+  let token: boolean;
+  // mock fetching auth token
+  token = (() => false)();
+
+  const options = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...(token && {
+        Authorization: `Bearer ${token}`,
+      }),
+    },
+    method: method,
+    ...(body && { body: JSON.stringify(body) }),
+  };
+
+  try {
+    const pathWithQuery = [path, query ? stringify(query) : undefined]
+      .filter(isDefined => isDefined)
+      .join('?');
+
+    const url = new URL(pathWithQuery, baseUrl);
+
+    console.log(`CALLING ${url.href}`);
+    const resp = await fetch(url.href, options);
+    const json = await resp.json();
+    return json;
+  } catch (error) {
+    return error;
+  }
+}
