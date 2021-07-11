1747605d44aba4494c6d6ff0a62a7efa7bbf9c2d
DPirate <milindd1996@gmail.com>
2021-07-11 20:30:46 +0530

    add timer component
     - to display time in Api Analytics

---
+++ src/components/Timer.tsx
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+
+export function Timer() {
+  const [time, setTime] = useState(moment());
+
+  useEffect(() => {
+    setTimeout(() => setTime(moment()), 1000);
+  }, [time]);
+
+  return (
+    <div>
+      <p>{time.toLocaleString()}</p>
+    </div>
+  );
+}
