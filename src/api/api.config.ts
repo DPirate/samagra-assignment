b70d5787ce497a3e534e015417a46876331ef307
DPirate <milindd1996@gmail.com>
2021-07-11 17:09:38 +0530

    add api config

---
+++ src/api/api.config.ts
@@ -0,0 +1,11 @@
+export const API_URL = 'https://jsonplaceholder.typicode.com/';
+
+export enum API_PATHS {
+  Todos = 'todos',
+  Comments = 'comments',
+}
+
+export enum QueryKeys {
+  GetTodo = 'GetTodo',
+  GetComments = 'GetComments',
+}
