87c13d3d161b143e74d865ddf4b2e6f7a203edcc
DPirate <milindd1996@gmail.com>
2021-07-12 09:43:41 +0530

    update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['react-app', 'react-app/jest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
